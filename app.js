8a7821cf7efc81d6634d5d489111322790ae3731
tasosp21 <tasosp21@gmail.com>
2021-09-05 17:15:55 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -9,12 +9,10 @@ const catchAsync = require('./utils/catchAsync');
 const session = require('express-session');
 const flash = require('connect-flash');
 const ExpressError = require('./utils/ExpressError');
-//const methodOverride = require('method-override');
+const methodOverride = require('method-override');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
-
-
 const userRoutes = require('./routes/users');
 
 //const cookie = require('cookie-parser);
@@ -48,7 +46,7 @@ app.set('views',path.join(__dirname, 'views'))
 
 
 app.use(express.urlencoded({ extended: true }));
-//app.use(methodOverride('_method'));
+app.use(methodOverride('_method'));
 //app.use(express.static(path.join(__dirname, 'public')))
 
 const sessionConfig = {
@@ -87,8 +85,10 @@ app.use('/', userRoutes);
 
 
 app.get('/', (req, res) => {
-    res.render('home')
+    res.render('/home')
 });
+
+
 
 
 app.all('*', (req, res, next) => {
@@ -101,85 +101,8 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err })
 })
 
+
+
 app.listen(3000, () => {
     console.log('Serving on port 3000')
 })
-
-
-
-
-
-
-
-
-
-
-
-
-//const User = require('./models/user');
-//const userRoutes = require('./routes/users');
-//const { search } = require('./routes/users');
-
-
-//Database connection
-//mongoose.connect('mongodb://localhost:27017/aek', {useNewUrlParser: true,useCreateIndex: true, useUnifiedTopology: true});
-//const db = mongoose.connection;
-//db.on("error", console.error.bind(console,"connection error:"))
-//db.once("open", () => {
-  //  console.log("Database connected");
-//})
-
-
-
-
-//app.use('/', userRoutes);
-
-
-
-
-//Passport libraries
-//app.use(passport.initialize());
-//app.use(passport.session());
-//passport.use(new LocalStrategy(User.authenticate()));
-//passport.serializeUser(User.serializeUser());
-//passport.deserializeUser(User.deserializeUser());
-
-
-
-
-//const sessionConfig = {
-  //  secret: 'thisshouldbeabettersecret!',
-  //  resave: false,
-  //  saveUnitialized: true,
-  //  cookie: {
-  //      httpOnly: true,
-  //      expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
-  //      maxAge: 1000 * 60 * 60 * 24 * 7
- //   }
-//}
-//app.use(session(sessionConfig))
-
-//app.use(flash());
-
-
-//middleware for every single request
-//app.use((req,res,next) => {
-  //  res.locals.success = req.flash('success');
-   // res.locals.error = req.flash('error');
-  //  next();
-//})
-
-
-//app.get('/', (req,res) => {
-  //  res.render('home')
-//})
-
-
-//app.use((err,req,res,next)=>{
- //   res.send('Something went wrong!')
-//})
-
-//application listening to server
-//app.listen(3000, (req,res)=> {
-  //  console.log('Serving on port 3000')
-//})
